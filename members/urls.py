6c2f77d05ba3315b5d6ff4600ecc14236b1dc301
Josh Lin <linynjosh@gmail.com>
2023-12-06 15:44:30 -0800

    Used built in generic views

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import MemberDeleteView, EditMemberView, AddMemberView, MemberListView
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("add_member", views.add_member, name="add_member"),
-    path("edit_member", views.edit_member, name="edit_member"),
-    path('delete_member', views.delete_member, name='delete_member'),
+    path("", MemberListView.as_view(), name="home"),
+    path("add_member", AddMemberView.as_view(), name="add_member"),
+    path('edit_member/<str:first_name>/<str:last_name>/', EditMemberView.as_view(), name='edit_member'),
+    path('delete_member', MemberDeleteView.as_view(), name='delete_member'),
     ]
--- members/views.py
+++ members/views.py
@@ -1,78 +1,89 @@
 from sqlite3 import IntegrityError
-from django.shortcuts import redirect, render
-from django.http import Http404, HttpResponseNotAllowed
-from django.views.decorators.http import require_http_methods
+from django.http import Http404
 from .models import Member
-from .forms import AddMemberForm, DeleteMemberForm, EditMemberForm
+from .forms import AddMemberForm, EditMemberForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView, FormView
+from django.views.generic import ListView
 
 # Create your views here.
 
-@require_http_methods(["GET"])
-def home(response):
-    members = Member.objects.all()
-    size = len(members)
-    return render(response, "main/list.html", {"members": members, "size": size})
+class MemberDeleteView(DeleteView):
+    template_name = ''
+    model = Member
+    success_url = reverse_lazy('home')  
 
-@require_http_methods(["POST"])
-def delete_member(request):
-    if request.method == 'POST':
-        form = DeleteMemberForm(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
+    def get_object(self, queryset=None):
+        first_name = self.request.POST.get('first_name', None)
+        last_name = self.request.POST.get('last_name', None)
+        return Member.objects.get(first_name=first_name, last_name=last_name)
 
-            try:
-                member = Member.objects.get(first_name=first_name, last_name=last_name)
-                member.delete()
-            except Member.DoesNotExist:
-                raise Http404("Member does not exist.")
-            
-        return redirect('home')
-    else:
-        return HttpResponseNotAllowed(['POST'])
+class MemberListView(ListView):
+    template_name = 'main/list.html'
+    model = Member
+    context_object_name = 'members'
 
-@require_http_methods(["POST"])
-def edit_member(request):
-    if request.method == "POST":
-        form = EditMemberForm(request.POST)  
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone = form.cleaned_data['phone_number']
-            email = form.cleaned_data['email']
-            is_admin = form.cleaned_data['is_admin']
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['size'] = len(context['members'])
+        return context
 
-            try:
-                member = Member.objects.get(first_name=first_name, last_name=last_name)
-                member.phone = phone
-                member.email = email
-                member.is_admin = is_admin
-                member.save()
-            except Member.DoesNotExist:
-                raise Http404("Member does not exist.")
-            
-        return redirect('home')
-    else:
-        return HttpResponseNotAllowed(['POST'])
+class EditMemberView(FormView):
+    template_name = '' 
+    form_class = EditMemberForm
+    success_url = reverse_lazy('home') 
 
-@require_http_methods(["POST"])
-def add_member(request):
-    if request.method == "POST":
-        form = AddMemberForm(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone = form.cleaned_data['phone_number']
-            email = form.cleaned_data['email']
-            is_admin = form.cleaned_data['is_admin']
+    def form_valid(self, form):
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        phone = form.cleaned_data['phone_number']
+        email = form.cleaned_data['email']
+        is_admin = form.cleaned_data['is_admin']
 
-            try:
-                member = Member(last_name=last_name, first_name=first_name, phone=phone, email=email, is_admin=is_admin)
-                member.save()
-            except IntegrityError:
-                form.add_error(None, "Member already exists in the database.")
-                
-        return redirect('home')           
-    else:
-        return HttpResponseNotAllowed(['POST'])
+        try:
+            member = Member.objects.get(first_name=first_name, last_name=last_name)
+            member.phone = phone
+            member.email = email
+            member.is_admin = is_admin
+            member.save()
+        except Member.DoesNotExist:
+            raise Http404("Member does not exist.")
 
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+    def get_initial(self):
+        first_name = self.kwargs.get('first_name')
+        last_name = self.kwargs.get('last_name')
+        return {'first_name': first_name, 'last_name': last_name}
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+class AddMemberView(FormView):
+    template_name = ''
+    form_class = AddMemberForm
+    success_url = reverse_lazy('home')  
+
+    def form_valid(self, form):
+        first_name = form.cleaned_data['first_name']
+        last_name = form.cleaned_data['last_name']
+        phone = form.cleaned_data['phone_number']
+        email = form.cleaned_data['email']
+        is_admin = form.cleaned_data['is_admin']
+
+        try:
+            member = Member.objects.create(last_name=last_name, first_name=first_name, phone=phone, email=email, is_admin=is_admin)
+        except IntegrityError:
+            form.add_error(None, "Member already exists in the database.")
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
